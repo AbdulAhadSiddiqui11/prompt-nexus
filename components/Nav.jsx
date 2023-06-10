3c978677c8f882004bf9eedfd7c13d2f69661df7
Abdul Ahad Siddiqui <abdulahadsiddiqui011@gmail.com>
2023-06-11 02:53:02 +0530

    Google OAuth, Next Auth, Mongo User Model and DB

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,7 +6,7 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
 
   const [providers, setProviders] = useState(null);
   const [toggleDropdown, setToggleDropdown] = useState(false);
@@ -23,7 +23,7 @@ const Nav = () => {
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
         <Image 
-          src="/assets/images/logo.svg"
+          src='/assets/images/logo.svg'
           alt="Prompt Nexus Logo"
           width={30}
           height={30}
@@ -35,7 +35,7 @@ const Nav = () => {
       {/* Desktop Nav */}
 
       <div className='sm:flex hidden'>
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex gap-3 md:gap-5">
 
             <Link href='/create-prompt' className="black_btn">
@@ -47,7 +47,7 @@ const Nav = () => {
             </button>
 
             <Link href='/profile'>
-              <Image src='/assets/images/logo.svg'
+              <Image src={session?.user.image}
                 width={37} height={37} className='rounded-full' alt='Profile' />
             </Link>
           </div>
@@ -64,10 +64,10 @@ const Nav = () => {
 
       {/* Mobile Nav */}
       <div className='sm:hidden flex relative'>
-      {isUserLoggedIn ? (
+      {session?.user ? (
           <div className="flex">
 
-              <Image src='/assets/images/logo.svg'
+              <Image src={session?.user.image}
                 width={37} 
                 height={37} 
                 className='rounded-full' 
